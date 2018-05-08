d5d970ab8eaae145cb33bbdf9653f949a7167859
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2018-05-07 17:57:55 -0700

    remove go file. (#2)

--- pkg/unused.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-package pkg
-
-// This file is unused but present because `dep` needs some go code present before restoring dependencies. Since we
-// use `dep` for some of our E2E testing, add this file, which we can remove once we add more go code back.
