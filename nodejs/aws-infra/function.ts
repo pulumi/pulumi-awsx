c65f7ef4eb83303db805fc5d724c2ae8712b5d01
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-17 17:05:06 -0700

    Simplify callback form.

--- nodejs/aws-infra/function.ts
+++ nodejs/aws-infra/function.ts
@@ -3,24 +3,16 @@
 import * as aws from "@pulumi/aws";
 
 /**
- * Shape of callbacks used for synchronous AWS lambda functions.  If a Node 6.x runtime is chosen
- * for the AWS lambda, these are the only callback shapes allowed.
+ * A synchronous or asynchronous callback.  Async callbacks are only supported with an AWS lambda
+ * runtime of 8.10 or higher.  On those runtimes a Promise can be returned and 'callback' can be
+ * ignored, and AWS will appropriately handle things.  For AWS lambda pre-8.10, a synchronous
+ * version of this must be used.  The callback should then return nothing, and should instead invoke
+ * 'callback' when complete.
  */
-export type SyncCallback<E, R> = (event: E, context: aws.serverless.Context, callback: (error: any, result: R) => void) => void;
-
-/**
- * Shape of callbacks used for asynchronous AWS lambda functions.  These are supported on AWS lambda
- * if a node runtime of 8.10 (the default runtime) or higher is used.
- */
-export type AsyncCallback<E, R> = (event: E, context?: aws.serverless.Context) => Promise<void>;
-
-/**
- * A synchronous or asynchronous callback.
- */
-export type Callback<E, R> = SyncCallback<E, R> | AsyncCallback<E, R>;
+export type Callback<E, R> = (event: E, context: aws.serverless.Context, callback: (error: any, result: R) => void) => Promise<void> | void;
 
 /**
  * Handler represents the appropriate type for functions that can take either an AWS lambda function
  * instance, or a JS function object that will then be used to create the AWS lambda function.
  */
-export type Handler<E, R> = aws.lambda.Function | Callback<E, R>;
+export type Handler<E, R> = Callback<E, R> | aws.lambda.Function;
