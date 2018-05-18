833a67c611f6c63140f33262b30e764ae6b8cc60
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-17 18:14:20 -0700

    Clean up docs.

--- nodejs/aws-infra/function.ts
+++ nodejs/aws-infra/function.ts
@@ -3,11 +3,11 @@
 import * as aws from "@pulumi/aws";
 
 /**
- * A synchronous or asynchronous callback.  Async callbacks are only supported with an AWS lambda
- * runtime of 8.10 or higher.  On those runtimes a Promise can be returned and 'callback' can be
- * ignored, and AWS will appropriately handle things.  For AWS lambda pre-8.10, a synchronous
- * version of this must be used.  The callback should then return nothing, and should instead invoke
- * 'callback' when complete.
+ * A synchronous or asynchronous function that can be converted into an AWS lambda.  Async callbacks
+ * are only supported with an AWS lambda runtime of 8.10 or higher.  On those runtimes a Promise can
+ * be returned, 'callback' parameter can be ignored, and AWS will appropriately handle things. For
+ * AWS lambda pre-8.10, a synchronous function must be provided.  The synchronous function should
+ * return nothing, and should instead invoke 'callback' when complete.
  */
 export type Callback<E, R> = (event: E, context: aws.serverless.Context, callback: (error: any, result: R) => void) => Promise<void> | void;
 
