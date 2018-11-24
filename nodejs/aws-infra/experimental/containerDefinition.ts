52355c682ff01a808a180bb222ced37ef0995c14
Cyrus Najmabadi <cyrusn@pulumi.com>
2018-11-24 14:36:22 -0800

    Arg work.

--- nodejs/aws-infra/experimental/containerDefinition.ts
+++ nodejs/aws-infra/experimental/containerDefinition.ts
@@ -19,33 +19,9 @@ import * as mod from ".";
 
 import * as utils from "../utils";
 
-export type ContainerDefinition = utils.Overwrite<utils.MakeInputs<aws.ecs.ContainerDefinition>, {
-    /**
-     * The image to use for the container.  If this is just a string, then the image will be pulled
-     * from the Docker Hub.  To provide customized image retrieval, provide [imageProvider] which
-     * can do whatever custom work is necessary and which should then update this
-     * ContainerDefinition appropriately.  See [ImageProvider] for common ways to create an image
-     * from a local docker build.
-     */
-    image?: pulumi.Input<string>
-
-    imageProvider?: mod.IImageProvider;
-
-    /**
-     * Provider that can produce a load balancer for this container.
-     */
-    loadBalancerProvider?: mod.LoadBalancerProvider;
-
-    // /**
-    //  * The port information to create a load balancer for.  At most one container in a service
-    //  * can have this set.  Should not be set for containers intended for TaskDeinitions that will
-    //  * just be run, and will not be part of an aws.ecs.Service.
-    //  */
-    // loadBalancerPort?: mod.ClusterLoadBalancerPort;
-}>;
-
 export type WrappedEndpoints = Record<string, Record<number, pulumi.Output<aws.apigateway.x.Endpoint>>>;
 
+/** @internal */
 export function computeContainerDefinition(
     parent: pulumi.Resource,
     name: string,
@@ -96,3 +72,80 @@ export function computeContainerDefinition(
         return containerDefinition;
     });
 }
+
+type WithoutUndefined<T> = T extends undefined ? never : T;
+
+type MakeInputs<T> = {
+    [P in keyof T]?: pulumi.Input<WithoutUndefined<T[P]>>;
+};
+
+// The shape we want for ContainerDefinitions.  We don't export this as 'Overwrite' types are not
+// pleasant to work with. However, they internally allow us to succinctly express the shape we're
+// trying to provide. Code later on will ensure these types are compatible.
+type OverwriteShape = utils.Overwrite<MakeInputs<aws.ecs.ContainerDefinition>, {
+    name: pulumi.Input<string>;
+    image?: pulumi.Input<string>
+    imageProvider?: mod.IImageProvider;
+    loadBalancerProvider?: mod.LoadBalancerProvider;
+}>;
+
+export interface ContainerDefinition {
+    // Properties from aws.ecs.ContainerDefinition
+    command?: pulumi.Input<string[]>;
+    cpu?: pulumi.Input<number>;
+    disableNetworking?: pulumi.Input<boolean>;
+    dnsSearchDomains?: pulumi.Input<string[]>;
+    dnsServers?: pulumi.Input<string[]>;
+    dockerLabels?: pulumi.Input<{
+        [label: string]: string;
+    }>;
+    dockerSecurityOptions?: pulumi.Input<string[]>;
+    entryPoint?: pulumi.Input<string[]>;
+    environment?: pulumi.Input<aws.ecs.KeyValuePair[]>;
+    essential?: pulumi.Input<boolean>;
+    extraHosts?: pulumi.Input<aws.ecs.HostEntry[]>;
+    hostname?: pulumi.Input<string>;
+    links?: pulumi.Input<string[]>;
+    linuxParameters?: pulumi.Input<aws.ecs.LinuxParameters>;
+    logConfiguration?: pulumi.Input<aws.ecs.LogConfiguration>;
+    memory?: pulumi.Input<number>;
+    memoryReservation?: pulumi.Input<number>;
+    mountPoints?: pulumi.Input<aws.ecs.MountPoint[]>;
+    portMappings?: pulumi.Input<aws.ecs.PortMapping[]>;
+    privileged?: pulumi.Input<boolean>;
+    readonlyRootFilesystem?: pulumi.Input<boolean>;
+    ulimits?: pulumi.Input<aws.ecs.Ulimit[]>;
+    user?: pulumi.Input<string>;
+    volumesFrom?: pulumi.Input<aws.ecs.VolumeFrom[]>;
+    workingDirectory?: pulumi.Input<string>;
+
+    // Changes made to core args type
+
+    name: pulumi.Input<string>;
+
+    /**
+     * The image to use for the container.  If this is just a string, then the image will be pulled
+     * from the Docker Hub.  To provide customized image retrieval, provide [imageProvider] which
+     * can do whatever custom work is necessary and which should then update this
+     * ContainerDefinition appropriately.  See [ImageProvider] for common ways to create an image
+     * from a local docker build.
+     */
+    image?: pulumi.Input<string>;
+
+    /**
+     * Provider that can produce the [image] and [environment] properties for this definition on
+     * demand.
+     */
+    imageProvider?: mod.IImageProvider;
+
+    /**
+     * Provider that can produce a load balancer for this container.
+     */
+    loadBalancerProvider?: mod.LoadBalancerProvider;
+}
+
+// Make sure our exported args shape is compatible with the overwrite shape we're trying to provide.
+let overwriteShape: OverwriteShape = undefined!;
+let argsShape: ContainerDefinition = undefined!;
+argsShape = overwriteShape;
+overwriteShape = argsShape;
