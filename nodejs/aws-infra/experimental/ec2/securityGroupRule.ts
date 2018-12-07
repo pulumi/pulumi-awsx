3fec4174c7838d739f05218740d06eb51b41e83e
Cyrus Najmabadi <cyrusn@pulumi.com>
2018-12-06 16:58:31 -0800

    Provide convenience entrypoints for making security groups.

--- nodejs/aws-infra/experimental/ec2/securityGroupRule.ts
+++ nodejs/aws-infra/experimental/ec2/securityGroupRule.ts
@@ -20,6 +20,145 @@ import { Network } from "./../../network";
 
 import * as utils from "./../../utils";
 
+export interface SecurityGroupRuleLocation {
+    /**
+     * List of CIDR blocks. Cannot be specified with `sourceSecurityGroupId`.
+     */
+    cidrBlocks?: pulumi.Input<pulumi.Input<string>[]>;
+
+    /**
+     * List of IPv6 CIDR blocks.
+     */
+    ipv6CidrBlocks?: pulumi.Input<pulumi.Input<string>[]>;
+
+    /**
+     * The security group id to allow access to/from, depending on the `type`. Cannot be specified
+     * with `cidrblocks`.
+     */
+    sourceSecurityGroupId?: pulumi.Input<string>;
+}
+
+export interface SecurityGroupRulePorts {
+    /**
+     * The protocol. If not icmp, tcp, udp, or all use the [protocol
+     * number](https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml)
+     */
+    protocol: pulumi.Input<PortInfoProtocol>;
+    /**
+     * The start port (or ICMP type number if protocol is "icmp").
+     */
+    fromPort: pulumi.Input<number>;
+    /**
+     * The end port (or ICMP code if protocol is "icmp").  Defaults to 'fromPort' if not specified.
+     */
+    toPort?: pulumi.Input<number>;
+}
+
+export type PortInfoProtocol = "-1" | "tcp" | "udp" | "icmp";
+
+export class AnyIPv4Location implements SecurityGroupRuleLocation {
+    public readonly cidrBlocks = ["0.0.0.0/0"];
+}
+
+export class AnyIPv6Location implements SecurityGroupRuleLocation {
+    public readonly ipv6CidrBlocks = ["::0/0"];
+}
+
+export class TcpPorts implements SecurityGroupRulePorts {
+    public readonly protocol = "tcp";
+    constructor(public readonly fromPort: pulumi.Input<number>,
+                public readonly toPort?: pulumi.Input<number>) {
+    }
+}
+
+const maxPort = 65535;
+
+export class AllTcpPorts extends TcpPorts {
+    constructor() {
+        super(0, maxPort);
+    }
+}
+
+export class UdpPorts implements SecurityGroupRulePorts {
+    public readonly protocol = "udp";
+    constructor(public readonly fromPort: pulumi.Input<number>,
+                public readonly toPort?: pulumi.Input<number>) {
+    }
+}
+
+export class AllUdpPorts extends UdpPorts {
+    constructor() {
+        super(0, maxPort);
+    }
+}
+
+export class IcmpPorts implements SecurityGroupRulePorts {
+    public readonly protocol = "icmp";
+    constructor(public readonly fromPort: pulumi.Input<number>,
+                public readonly toPort?: pulumi.Input<number>) {
+    }
+}
+
+export class AllTraffic implements SecurityGroupRulePorts {
+    public readonly protocol = "-1";
+    public readonly fromPort = 0;
+    public readonly toPort = 0;
+}
+
+export class SecurityGroupRules {
+    public static egressArgs(
+            destination: SecurityGroupRuleLocation,
+            ports: SecurityGroupRulePorts,
+            description?: pulumi.Input<string>): EgressSecurityGroupRuleArgs {
+        return SecurityGroupRules.createArgs(destination, ports, description);
+    }
+
+    public static ingressArgs(
+            source: SecurityGroupRuleLocation,
+            ports: SecurityGroupRulePorts,
+            description?: pulumi.Input<string>): IngressSecurityGroupRuleArgs {
+        return SecurityGroupRules.createArgs(source, ports, description);
+    }
+
+    private static createArgs(
+            location: SecurityGroupRuleLocation,
+            ports: SecurityGroupRulePorts,
+            description?: pulumi.Input<string>): EgressSecurityGroupRuleArgs & IngressSecurityGroupRuleArgs {
+        return {
+            ...location,
+            ...ports,
+            toPort: utils.ifUndefined(ports.toPort, ports.fromPort),
+            description,
+        };
+    }
+
+    public static egress(
+        name: string, securityGroup: x.ec2.SecurityGroup,
+        destination: SecurityGroupRuleLocation,
+        ports: SecurityGroupRulePorts,
+        description?: pulumi.Input<string>,
+        opts?: pulumi.ComponentResourceOptions) {
+
+        return new EgressSecurityGroupRule(
+            name, securityGroup,
+            SecurityGroupRules.egressArgs(destination, ports, description),
+            opts);
+    }
+
+    public static ingress(
+        name: string, securityGroup: x.ec2.SecurityGroup,
+        source: SecurityGroupRuleLocation,
+        ports: SecurityGroupRulePorts,
+        description?: pulumi.Input<string>,
+        opts?: pulumi.ComponentResourceOptions) {
+
+        return new IngressSecurityGroupRule(
+            name, securityGroup,
+            SecurityGroupRules.ingressArgs(source, ports, description),
+            opts);
+    }
+}
+
 export abstract class SecurityGroupRule extends pulumi.ComponentResource {
     public readonly instance: aws.ec2.SecurityGroupRule;
     public readonly securityGroup: x.ec2.SecurityGroup;
