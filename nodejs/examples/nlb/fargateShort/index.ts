f9b7802e697e11e377896f548b308d217850fdf5
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-22 11:28:46 -0700

    Break out short/long tests for fargate.

---
+++ nodejs/examples/nlb/fargateShort/index.ts
@@ -0,0 +1,73 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+const vpc = awsx.ec2.Vpc.getDefault();
+const cluster = new awsx.ecs.Cluster("testing", { vpc });
+
+// A simple NGINX service, scaled out over two containers.
+const nginxListener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port: 80 });
+
+// A simple NGINX service, scaled out over two containers, starting with a task definition.
+const simpleNginxListener = new awsx.elasticloadbalancingv2.NetworkListener("simple-nginx", { port: 80 });
+const simpleNginx = new awsx.ecs.FargateTaskDefinition("simple-nginx", {
+    container: {
+        image: "nginx",
+        memory: 128,
+        portMappings: [simpleNginxListener],
+    },
+}).createService("simple-nginx", { cluster, desiredCount: 2});
+
+
+function errorJSON(err: any) {
+    const result: any = Object.create(null);
+    Object.getOwnPropertyNames(err).forEach(key => result[key] = err[key]);
+    result.florp = "blopr";
+    return result;
+}
+
+function handleError(err: Error) {
+    console.error(errorJSON(err));
+    return {
+        statusCode: 500,
+        body: JSON.stringify(errorJSON(err)),
+    };
+}
+
+// expose some APIs meant for testing purposes.
+const api = new awsx.apigateway.API("containers", {
+    routes: [{
+        path: "/test",
+        method: "GET",
+        eventHandler: async (req) => {
+            try {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        nginx: nginxListener.endpoint.get(),
+                    }),
+                };
+            } catch (err) {
+                return handleError(err);
+            }
+        },
+    }, {
+        path: "/nginx",
+        target: nginxListener,
+    }],
+});
+
+export let frontendURL = api.url;
