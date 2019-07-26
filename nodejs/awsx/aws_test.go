dc1e25ed3714b8813bc49ab4da55491a0fda6c3e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-26 15:00:35 -0700

    Run again

--- nodejs/awsx/aws_test.go
+++ nodejs/awsx/aws_test.go
@@ -62,33 +62,33 @@ func Test_Examples(t *testing.T) {
 	}
 
 	shortTests := []integration.ProgramTestOptions{
-		// integration.ProgramTestOptions{
-		// 	Config: map[string]string{"aws:region": envRegion},
-		// 	Dir:    path.Join(cwd, "../examples/cluster"),
-		// 	Dependencies: []string{
-		// 		"@pulumi/awsx",
-		// 	},
-		// 	Quick:       true,
-		// 	SkipRefresh: true,
-		// },
-		// testBase.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "../examples/dashboards"),
-		// }),
-		// testBase.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "../examples/ecr"),
-		// }),
-		// testBase.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "../examples/metrics"),
-		// }),
-		// testBase.With(integration.ProgramTestOptions{
-		// 	Dir:       path.Join(cwd, "../examples/vpc"),
-		// 	StackName: addRandomSuffix("vpc"),
-		// }),
-		// testBase.With(integration.ProgramTestOptions{
-		// 	Dir:                    path.Join(cwd, "../examples/nlb/fargateShort"),
-		// 	StackName:              addRandomSuffix("fargate"),
-		// 	ExtraRuntimeValidation: containersRuntimeValidator(envRegion, true /*isFargate*/, true /*short*/),
-		// }),
+		integration.ProgramTestOptions{
+			Config: map[string]string{"aws:region": envRegion},
+			Dir:    path.Join(cwd, "../examples/cluster"),
+			Dependencies: []string{
+				"@pulumi/awsx",
+			},
+			Quick:       true,
+			SkipRefresh: true,
+		},
+		testBase.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "../examples/dashboards"),
+		}),
+		testBase.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "../examples/ecr"),
+		}),
+		testBase.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "../examples/metrics"),
+		}),
+		testBase.With(integration.ProgramTestOptions{
+			Dir:       path.Join(cwd, "../examples/vpc"),
+			StackName: addRandomSuffix("vpc"),
+		}),
+		testBase.With(integration.ProgramTestOptions{
+			Dir:                    path.Join(cwd, "../examples/nlb/fargateShort"),
+			StackName:              addRandomSuffix("fargate"),
+			ExtraRuntimeValidation: containersRuntimeValidator(envRegion, true /*isFargate*/, true /*short*/),
+		}),
 		testBase.With(integration.ProgramTestOptions{
 			Dir: path.Join(cwd, "../examples/api"),
 			Dependencies: []string{
