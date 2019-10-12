a02c6d9301c12e07caebdb8117f5f06e67efd75f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-10-11 18:59:10 -0700

    Cleanup

--- nodejs/awsx/ecs/service.ts
+++ nodejs/awsx/ecs/service.ts
@@ -90,7 +90,6 @@ function getLoadBalancers(service: ecs.Service, name: string, args: ServiceArgs)
 
         for (const obj of container.portMappings) {
             if (x.ecs.isContainerLoadBalancerProvider(obj)) {
-                console.log("portMapping: " + containerName);
                 containerLoadBalancerProviders.set(containerName, obj);
             }
         }
@@ -99,7 +98,6 @@ function getLoadBalancers(service: ecs.Service, name: string, args: ServiceArgs)
     // Finally see if we were directly given load balancing listeners to associate our containers
     // with. If so, use their information to populate our LB information.
     for (const containerName of Object.keys(service.listeners)) {
-        console.log("listener: " + containerName);
         containerLoadBalancerProviders.set(containerName, service.listeners[containerName]);
     }
 
