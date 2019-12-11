e96b11de8d6a5c5df8d7274b4c727d6bcd945770
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-10 20:01:07 -0800

    Revert

--- nodejs/examples/nlb/fargate/index.ts
+++ nodejs/examples/nlb/fargate/index.ts
@@ -321,6 +321,6 @@ const api = new awsx.apigateway.API("containers", {
 
 export let frontendURL = api.url;
 export let fargateVpcId = vpc.id;
-export let fargatePublicSubnets = vpc.publicSubnetsIds;
+export let fargatePublicSubnets = vpc.publicSubnetIds;
 export let fargatePrivateSubnets = vpc.privateSubnetIds;
 export let fargateIsolatedSubnets = vpc.isolatedSubnetIds;
