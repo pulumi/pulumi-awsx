f8cb8f3c6a4fb26c408e5f8d216e292caad189f3
Akhan Zhakiyanov <ahanoff@gmail.com>
2020-12-25 02:22:39 +0800

    Update taskDefinition.ts

--- nodejs/awsx/ecs/taskDefinition.ts
+++ nodejs/awsx/ecs/taskDefinition.ts
@@ -147,9 +147,9 @@ export abstract class TaskDefinition extends pulumi.ComponentResource {
     public static defaultTaskRolePolicyARNs() {
         return [
             // Provides wide access to "serverless" services (Dynamo, S3, etc.)
-            aws.iam.ManagedPolicies.AWSLambdaFullAccess,
+            aws.iam.ManagedPolicy.AWSLambdaFullAccess,
             // Required for lambda compute to be able to run Tasks
-            aws.iam.ManagedPolicies.AmazonEC2ContainerServiceFullAccess,
+            aws.iam.ManagedPolicy.AmazonECSFullAccess,
         ];
     }
 
