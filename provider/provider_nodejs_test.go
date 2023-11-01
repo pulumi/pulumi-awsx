e5b3c5fbb2b7a08439f2ac1e0869d253e8d946b3
Daniel Bradley <daniel@pulumi.com>
2023-11-01 14:10:25 +0000

    Record ts-lb-attach-ec2 upgrade test
    - Replace peer-dependency with normal dependency.

--- provider/provider_nodejs_test.go
+++ provider/provider_nodejs_test.go
@@ -50,7 +50,7 @@ func TestExamples(t *testing.T) {
 		//
 		// "ts-lb-with-subnets",
 		// "ts-lb-attach-lambda",
-		// "ts-lb-attach-ec2",
+		"ts-lb-attach-ec2",
 		// "vpc/nodejs/custom-provider",
 		// "vpc/nodejs/specific-vpc-layout",
 		// "vpc/nodejs/vpc-with-service-endpoint",
