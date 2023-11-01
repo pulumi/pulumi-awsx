fdb1ece65cad12e24dc17318b2efa79a6f4d90d3
Daniel Bradley <daniel@pulumi.com>
2023-11-01 15:06:49 +0000

    Force upgrade tests to always run in the same region
    Ignore local configuration otherwise recordings might be in a different region and will force all resources to be replaced.

--- provider/provider_nodejs_test.go
+++ provider/provider_nodejs_test.go
@@ -75,6 +75,9 @@ func test(dir string, opts ...providertest.Option) *providertest.ProviderTest {
 
 		providertest.WithBaselineVersion("1.0.6"),
 
+		// Ensure we use the same region for all tests - when recording and replaying.
+		providertest.WithConfig("aws:region", "us-west-2"),
+
 		providertest.WithExtraBaselineDependencies(map[string]string{
 			"aws": "5.42.0",
 		}),
