64914b9d622025a597c51a4a729d57a7e464d9d4
larrywax <giuseppedanna@unobravo.com>
2024-02-26 13:01:16 +0100

    wip

--- awsx/schema-types.ts
+++ awsx/schema-types.ts
@@ -115,6 +115,7 @@ export interface ImageArgs {
     readonly platform?: pulumi.Input<string>;
     readonly repositoryUrl: pulumi.Input<string>;
     readonly target?: pulumi.Input<string>;
+    readonly imageTag?: pulumi.Input<string>;
 }
 export abstract class Repository<TData = any> extends (pulumi.ComponentResource)<TData> {
     public lifecyclePolicy?: aws.ecr.LifecyclePolicy | pulumi.Output<aws.ecr.LifecyclePolicy>;
