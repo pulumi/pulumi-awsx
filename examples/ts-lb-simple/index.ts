77c5805d43f599f3ee13d741230363082bb1bf95
Florian Stadler <florian@pulumi.com>
2024-05-14 13:10:27 +0200

    Rework LB examples to not rely on invokes

--- examples/ts-lb-simple/index.ts
+++ examples/ts-lb-simple/index.ts
@@ -2,14 +2,51 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+// Create VPC for the LB and ECS service
+const vpc = new awsx.ec2.Vpc("test-vpc", {
+    subnetSpecs: [
+      { type: "Public" }
+    ],
+    natGateways: {
+      strategy: "None",
+    }
+  });
+
 const cluster = new aws.ecs.Cluster("default-cluster");
 
-// // Create a load balancer on port 80 and spin up two instances of Nginx.
-const lb = new awsx.lb.ApplicationLoadBalancer("nginx-lb");
+// Create a load balancer on port 80 and spin up two instances of Nginx.
+const lb = new awsx.lb.ApplicationLoadBalancer("nginx-lb", {
+    subnets: vpc.subnets,
+  });
+
+// Create a security group for the service that only allows ingress from the LB
+const serviceSg = new aws.ec2.SecurityGroup(`test-service-sg`, {
+    vpcId: vpc.vpcId,
+    ingress: [
+      {
+        protocol: '-1',
+        fromPort: 0,
+        toPort: 0,
+        securityGroups: [lb.defaultSecurityGroup.apply(sg => sg!.id)],
+      },
+    ],
+    egress: [
+      {
+        protocol: '-1',
+        fromPort: 0,
+        toPort: 0,
+        cidrBlocks: ['0.0.0.0/0'],
+      }
+    ]
+  });
 
 const service = new awsx.ecs.FargateService("my-service", {
     cluster: cluster.arn,
-    assignPublicIp: true,
+    networkConfiguration: {
+        assignPublicIp: true,
+        securityGroups: [serviceSg.id],
+        subnets: vpc.publicSubnetIds,
+      },
     taskDefinitionArgs: {
         container: {
             image: "nginx:latest",
--- examples/ts-nlb-simple/index.ts
+++ examples/ts-nlb-simple/index.ts
@@ -1,4 +1,16 @@
 import * as awsx from "@pulumi/awsx";
 
-// // Create a network load balancer.
-const lb = new awsx.lb.NetworkLoadBalancer("nginx-lb");
+// Create VPC for the LB
+const vpc = new awsx.ec2.Vpc("test-vpc", {
+    subnetSpecs: [
+      { type: "Public" }
+    ],
+    natGateways: {
+      strategy: "None",
+    }
+  });
+
+// Create a network load balancer.
+const lb = new awsx.lb.NetworkLoadBalancer("nginx-lb", {
+    subnets: vpc.subnets,
+});
