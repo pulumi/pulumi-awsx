bb47b8646592fa8f58e987a7d00a689621d65b59
Anton Tayanovskyy <anton@pulumi.com>
2024-11-05 17:32:40 -0500

    Improve the script for upgrading AWS

--- scripts/upgrade-aws.sh
+++ scripts/upgrade-aws.sh
@@ -10,6 +10,11 @@ VER="$1"
 
 echo "V=$VER"
 
-(cd awsx && yarn add "@pulumi/aws@$VER")
+# Strips the v from the version to get the correct npm version.
+(cd awsx && yarn upgrade @pulumi/aws@${VER#v})
 
-make build_sdks
+# Deduplicate the dependencies.
+(cd awsx && yarn run dedupe-deps)
+
+# Ensure that we don't have any duplicate dependencies.
+(cd awsx && yarn run check-duplicate-deps)
