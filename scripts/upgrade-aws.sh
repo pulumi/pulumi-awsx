fa40885863c4ac51df87fed50daecaf12ba91bcc
Anton Tayanovskyy <anton@pulumi.com>
2024-11-05 17:38:18 -0500

    Bring back rebuilding the SDKs

--- scripts/upgrade-aws.sh
+++ scripts/upgrade-aws.sh
@@ -18,3 +18,6 @@ echo "V=$VER"
 
 # Ensure that we don't have any duplicate dependencies.
 (cd awsx && yarn run check-duplicate-deps)
+
+# Rebulid the SDKs, which will also rebuild the schema and all other files.
+make build_sdks
