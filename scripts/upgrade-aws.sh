2f0d64bd5405b08760b427508935db794afcbcfd
Anton Tayanovskyy <anton@pulumi.com>
2024-11-06 09:13:33 -0500

    Automate PRs that update the pulumi-aws dependency (#1405)
    Adds automation to automatically stand up PRs that update the pulumi-aws
    dependency.
    
    Example PR: https://github.com/pulumi/pulumi-awsx/pull/1406

--- scripts/upgrade-aws.sh
+++ scripts/upgrade-aws.sh
@@ -10,6 +10,14 @@ VER="$1"
 
 echo "V=$VER"
 
-(cd awsx && yarn add "@pulumi/aws@$VER")
+# Strips the v from the version to get the correct npm version.
+(cd awsx && yarn upgrade @pulumi/aws@${VER#v})
 
+# Deduplicate the dependencies.
+(cd awsx && yarn run dedupe-deps)
+
+# Ensure that we don't have any duplicate dependencies.
+(cd awsx && yarn run check-duplicate-deps)
+
+# Rebulid the SDKs, which will also rebuild the schema and all other files.
 make build_sdks
