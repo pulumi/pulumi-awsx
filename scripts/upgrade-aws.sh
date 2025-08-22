e174d5710d403d47064a25ab9117a21c5751b1c4
Bryce Lampe <bryce@pulumi.com>
2025-08-22 13:50:27 -0700

    Fix upgrade-aws job

--- scripts/upgrade-aws.sh
+++ scripts/upgrade-aws.sh
@@ -19,10 +19,12 @@ echo "V=$VER"
 # Ensure that we don't have any duplicate dependencies.
 (cd awsx && yarn run check-duplicate-deps)
 
+# Upgrade our SDK go dependency.
+(cd sdk && go get -u github.com/pulumi/pulumi-aws/sdk/v7)
+
 # Rebuild provider internals such as awsx/schema-types.ts
 # We need to run this before rebuilding the SDKs or they may be missing types
 make provider
 
 # Rebulid the SDKs, which will also rebuild the schema and all other files.
 make build_sdks
-
