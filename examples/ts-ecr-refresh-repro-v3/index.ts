918e2bef7fb04cd91459ae3ffd0dbda7c8024bb9
corymhall <43035978+corymhall@users.noreply.github.com>
2026-04-15 12:45:11 -0400

    Convert ecr refresh repro to yaml

--- examples/ts-ecr-refresh-repro-v3/index.ts
+++
@@ -1,9 +0,0 @@
-import * as awsx from "@pulumi/awsx";
-
-const repository = new awsx.ecr.Repository("repository", { forceDelete: true });
-
-export const repositoryName = repository.repository.name;
-export const image = new awsx.ecr.Image("image", {
-  repositoryUrl: repository.repository.repositoryUrl,
-  context: "./app",
-}).imageUri;
