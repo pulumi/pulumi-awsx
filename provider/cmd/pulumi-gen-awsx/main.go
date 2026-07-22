364111d62c4602206a6122d46877f72a88711afb
Cory Hall <chall@pulumi.com>
2026-07-22 09:28:01 +0000

    Fix pulumi-gen-awsx build for pulumi v3.253.0
    pulumi v3.253.0 moved provider RPC setup code into the SDK module, and
    pkg/v3/host.New, schema.NewPluginLoader, and schema.NewLoaderServerFromContext
    now use the pkg/v3/resource/plugin types. Import plugin from
    pkg/v3/resource/plugin instead of sdk/v3/go/common/resource/plugin so
    NewContext accepts the host and produces the Context type the loader expects.

--- provider/cmd/pulumi-gen-awsx/main.go
+++ provider/cmd/pulumi-gen-awsx/main.go
@@ -36,9 +36,9 @@ import (
 	pygen "github.com/pulumi/pulumi/pkg/v3/codegen/python"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	pkghost "github.com/pulumi/pulumi/pkg/v3/host"
+	"github.com/pulumi/pulumi/pkg/v3/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 
 	gen "github.com/pulumi/pulumi-awsx/provider/v3/pkg/schemagen"
 	"github.com/pulumi/pulumi-awsx/provider/v3/pkg/version"
